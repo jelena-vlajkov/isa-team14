eb47ac6d3ce2880c44d8edf8d4fcc9b63f719dca
Jelena Vlajkov <vlajkovj31@gmail.com>
2020-11-20 20:18:39 +0100

    :tada: Spring-boot application initialized

---
+++ atlaspharmacy/src/main/java/com/atlaspharmacy/atlaspharmacy/AtlaspharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.atlaspharmacy.atlaspharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlaspharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlaspharmacyApplication.class, args);
+	}
+
+}
---
+++ atlaspharmacy/src/test/java/com/atlaspharmacy/atlaspharmacy/AtlaspharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlaspharmacy.atlaspharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlaspharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
