5fd016412a184ac8729b10c6ed7037711e9f350c
Coa <alexignjat1998@gmail.com>
2021-02-02 23:27:07 +0100

    :lipstick: Changed fronted for pharmacy admin registration

---
+++ frontend/src/app/register-dermatologist/register-dermatologist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-dermatologist',
+  templateUrl: './register-dermatologist.component.html',
+  styleUrls: ['./register-dermatologist.component.css']
+})
+export class RegisterDermatologistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
