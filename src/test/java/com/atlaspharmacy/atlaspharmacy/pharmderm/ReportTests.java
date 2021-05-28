c031ca6742750e163c276b04d61ebe4cbab4a509
jelena-vlajkov <vlajkovj31@gmail.com>
2021-05-28 13:44:17 +0200

    :white_check_mark: Adding tests

---
+++ src/test/java/com/atlaspharmacy/atlaspharmacy/pharmderm/ReportTests.java
@@ -0,0 +1,16 @@
+package com.atlaspharmacy.atlaspharmacy.pharmderm;
+
+import org.junit.jupiter.api.Test;
+
+public class ReportTests {
+
+    @Test
+    void testSaveReport() {
+
+    }
+
+    @Test
+    void createPenalty() {
+        
+    }
+}
